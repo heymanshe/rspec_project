75a9f86cd7da0a686eff7b9bf0549480e23d63d6
heymanshe <himanshiksharma@outlook.com>
2025-06-19 19:38:53 +0530

    Update model for age validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     validates :name, :email, :age, presence: true
     validates :email, uniqueness: true
-    validates :age, numericality: { greater_than: 0 }
+    validates :age, numericality: { 
+        greater_than: 18,
+        less_than: 65,
+        only_integer: true
+    }
 end
