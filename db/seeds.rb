34b5df3b980eaf42cc2d88cd71b093a98a2ec057
heymanshe <himanshiksharma@outlook.com>
2025-06-19 16:19:58 +0530

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.find_or_create_by!(name: "John Doe", email: "john@example.com", age: 30) do |user|
+  user.name = "John Doe"
+  user.email = "john@example.com"
+  user.age = 30
+end
+
+10.times do
+    User.find_or_create_by!(name: Faker::Name.name, email: Faker::Internet.email, age: Faker::Number.between(from: 18, to: 65)) do |user|
+        user.name = Faker::Name.name
+        user.email = Faker::Internet.email
+        user.age = Faker::Number.between(from: 18, to: 65)
+    end
+end
+
+
