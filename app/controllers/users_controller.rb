2a762499c40263bd8085bfa42e4e69fba9a43bf5
heymanshe <himanshiksharma@outlook.com>
2025-06-19 17:07:52 +0530

    Fix rubocop issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
-require 'csv'
+require "csv"
 
 class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -22,21 +22,21 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-  
+
   def download_csv
     @users = User.all
-  
+
     respond_to do |format|
       format.csv do
-        response.headers['Content-Type'] = 'text/csv'
-        response.headers['Content-Disposition'] = 'attachment; filename=users.csv'
-        render template: 'users/download_csv'
+        response.headers["Content-Type"] = "text/csv"
+        response.headers["Content-Disposition"] = "attachment; filename=users.csv"
+        render template: "users/download_csv"
       end
     end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :age)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,5 +21,3 @@ end
         user.age = Faker::Number.between(from: 18, to: 65)
     end
 end
-
-
